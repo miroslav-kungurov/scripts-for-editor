5b9557f8a2a55cea630765a54b098d214084b868
Miroslav Kungurov <56649199+miroslav-kungurov@users.noreply.github.com>
2024-07-07 11:10:16 +0300

    add

---
+++ merge_json_with_same_structure.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+def merge_json_files(file_list, output_file):
+    merged_data = []
+
+    for file_name in file_list:
+        try:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                merged_data.extend(data)
+        except json.JSONDecodeError as e:
+            print(f"Ошибка декодирования JSON в файле {file_name}: {e}")
+        except Exception as e:
+            print(f"Произошла ошибка при обработке файла {file_name}: {e}")
+
+    with open(output_file, 'w', encoding='utf-8') as output:
+        json.dump(merged_data, output, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    # Список JSON файлов для объединения
+    json_files = ["habr_articles0.json", "habr_articles.json"]
+    # Имя выходного файла
+    output_json = "merged.json"
+
+    merge_json_files(json_files, output_json)
+    print(f"Файлы успешно объединены в {output_json}")
